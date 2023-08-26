263fb42a16108ed5b48a7157eb112505ebe821db
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-08-26 13:19:17 +0700

    update

---
+++ 14. SEARCHING/index.php
@@ -0,0 +1,82 @@
+<?php
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if( isset($_POST["cari"]) ) {
+    $mahasiswa = cari($_POST["keyword"]);
+    
+}
+
+// ambil data dari tabel mahasiswa / query data mahasiswa
+// $result = mysqli_query($db, "SELECT * FROM mahasiswa");
+
+// ambil data (fetch) mahasiswa dari object result
+// mysqli_fetch_row() // mengembalikan array numerik
+// mysqli_fetch_assoc() // mengembalikan array associative
+// mysqli_fetch_array() // mengembalikan keduanya
+// mysqli_fetch_object() // 
+
+// while( $mhs = mysqli_fetch_assoc($result) ) {
+// var_dump($mhs); 
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+
+<h1>Daftar Mahasiswa</h1>
+
+<a href="tambah.php">Tambah data mahasiswa</a>
+<br><br>
+
+<form action="" method="post">
+
+    <input type="text" name="keyword" size="40" 
+    autofocus placeholder="masukkan keyword pencarian.."
+    autocomplete="off">
+    <button type="submit" name="cari">Cari!</button>
+    <a href="index.php"><button type="submit" name="clear">Clear</button></a>
+</form>
+
+<br>
+
+<table border="1" cellpadding="10" cellspacing="0">
+<tr>
+    <th>No.</th>
+    <th>Aksi</th>
+    <th>Gambar</th>
+    <th>NRP</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Jurusan</th>
+</tr>
+
+<tr>
+    <?php $i = 1; ?>
+    <?php foreach( $mahasiswa as $row ) : ?>
+    <tr>
+        <td><center><?= $i; ?></center></td>
+    <td>
+        <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+        <a href="hapus.php?id=<?= $row["id"]; ?>">hapus</a>
+    </td>
+    <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="50" height="50"></td>
+    <td><?= $row["nrp"]; ?></td>
+    <td><?= $row["nama"]; ?></td>
+    <td><?= $row["email"]; ?></td>
+    <td><?= $row["jurusan"]; ?></td>
+</tr>
+<?php $i++ ?>
+<?php endforeach; ?>
+</table>
+
+</body>
+</html>
---
+++ 14. SEARCHING/phpdasar.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 25, 2023 at 11:02 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpdasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `email`, `jurusan`, `gambar`) VALUES
+(1, '0000007', 'Sholahuddin Alfatah', 'alfatah1505@gmail.com', 'Teknik Informatika', 'alfatah.jpeg'),
+(2, '0000008', 'Wahyoe  ', 'wahyoe@gmail.com ', 'Teknik Industri  ', 'wahyoe.png'),
+(3, '0000009', 'Rahmad      ', 'rahmad@gmail.com', 'Teknik Informatika', 'rahmad.jpeg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
